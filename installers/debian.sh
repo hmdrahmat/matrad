f2fb19e10883f8d3186f59680c46e3b4db84e2d4
Kim Hajin <64860741+hmdrahmat@users.noreply.github.com>
2024-02-25 17:07:20 +0700

    Update debian.sh

--- installers/debian.sh
+++ installers/debian.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 
-echo -e "Starting Debian installer Script..."
+echo -e "Starting Ubuntu installer Script..."
 
 MYSQL_ROOT_PASSWORD=$1
 
 #updating packages
 echo -e "Updating package lists.."
 sudo apt-get -y update
-
-#install Ngnix
-echo -e "Installing Ngnix server..."
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx Full'
-
-
-#add ondrej PPA
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get update
-
-#install Mysql server
-echo -e "Installing PHP-FPM and Mysql extension for PHP..."
-sudo apt-get -y install php7.4-fpm php7.4-mysql
